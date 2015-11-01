f7647bc66a78b2bc00ead4a0d2109951034f82a6
Benito <sanchez.benitog@gmail.com>
2015-10-31 23:37:56 -0700

    This file works great! They only negative thing is that it take a while to get installed.

---
+++ ionic.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+
+
+sudo apt-get install curl
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+
+sudo apt-get install nodejs
+
+sudo apt-get install build-essential
+
+sudo npm install -g cordova
