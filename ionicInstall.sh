a43bb5b83b7ca2c2737d732eb698123870a5e0fd
bennyg95 <sanchezbenitog@gmail.com>
2015-10-31 21:51:02 -0700

    Still needs work

--- ionicInstall.sh
+++ ionicInstall.sh
@@ -22,26 +22,26 @@ echo "Downloading nodejs"
 
 cd ~/Downloads
 
-if ['getconf LONG_BIT' == "64"]; then
+if [ `getconf LONG_BIT` = "64" ]
+	then
     wget "http://nodejs.org/dist/v4.2.1/node-v4.2.1-linux-x64.tar.gz"
 
-    tar xvzf "node-v4.2.1-linux-x64.tar.gz"
+    tar zxf "node-v4.2.1-linux-x64.tar.gz"
 
 else
 
     wget "http://nodejs.org/dist/v4.2.1/node-v4.2.1-linux-x86.tar.gz"
 
-    tar xvzf "node-v4.2.1-linux-x86.tar.gz"
+    tar zxf "node-v4.2.1-linux-x86.tar.gz"
 
 cd
 
 fi
 
-
 #install apache cordova
 clear
 echo "Installing cordova"
-sudo npm install -g cordova
+npm install -g cordova
 
 clear
 echo "update cordova"
@@ -54,7 +54,7 @@ sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0
 #install ionic
 clear
 echo "Installing ionic"
-sudo npm install -g ionic
+npm install -g ionic
 
 #clean up
 clear
