dfe591fe71822ae3b987f1cf5db691e11e466e4c
bennyg95 <sanchezbenitog@gmail.com>
2015-11-02 22:17:43 -0800

    Combined Debian and RedHat installation to one file.

--- fedoraTesting.sh
+++
@@ -1,7 +0,0 @@
-sudo yum install curl
-
-sudo yum install nodejs
-
-sudo yum install npm
-
-sudo npm install -g cordova ionic
--- ionic.sh
+++ ionic.sh
@@ -1,13 +1,26 @@
 #/bin/sh
 
 
+if [ -f /etc/debian_version ]; then
 
-sudo apt-get install curl
+	sudo apt-get install curl
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+	curl -sL https://deb.nodesource.com/setup | sudo bash -
 
-sudo apt-get install nodejs
+	sudo apt-get install nodejs
 
-sudo apt-get install build-essential
+	sudo apt-get install build-essential
 
-sudo npm install -g cordova ionic
+	sudo npm install -g cordova ionic
+
+elif [ -f /etc/redhat-release ]; then
+
+	sudo yum install curl
+
+	sudo yum install nodejs
+
+	sudo yum install npm
+
+	sudo npm install -g cordova ionic
+
+fi
